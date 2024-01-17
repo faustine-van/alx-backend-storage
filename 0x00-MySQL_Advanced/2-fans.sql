3a64f565804a07f10dbe65690d21f6324b654dfc
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-17 19:47:37 +0200

    updated

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,6 @@
+-- script that ranks country origins of bands, ordered by the number of (non-unique) fans
+-- Column names must be: origin and nb_fans
+SELECT origin, COUNT(*) AS nb_fans
+FROM metal_bands
+GROUP BY(origin)
+ORDER BY(nb_fans) DESC;
