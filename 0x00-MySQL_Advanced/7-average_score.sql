993fac8c95523e78e8d2467243c5a9837dbdb303
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-18 16:24:38 +0200

    update

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -9,7 +9,7 @@ BEGIN
   DECLARE average_score DECIMAL(10, 2);
 
   -- caculate or computes average  of a scores column for students.
-  SELECT AVG(score)INTO average_score FROM corrections WHERE user_id = user_id;
+  SELECT AVG(score) INTO average_score FROM corrections WHERE user_id = user_id;
 
   -- store the average score for a student
   UPDATE users SET average_score = average_score WHERE id = user_id;
