64f5da040cb1034ea2471133a04083ce5f2d19a0
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-18 22:23:51 +0200

    fixed variable naming conflict

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,17 +1,17 @@
 -- SQL script that creates a stored procedure ComputeAverageScoreForUser
 -- that computes and store the average score for a student.
 -- NB: An average score can be a decimal
-
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
+
+CREATE PROCEDURE ComputeAverageScoreForUser(input_id INT)
 BEGIN
   -- create varibale to store average
-  DECLARE average_score DECIMAL(10, 2);
-
+  DECLARE average DECIMAL(10, 2);
+  
   -- caculate or computes average  of a scores column for students.
-  SELECT AVG(score) INTO average_score FROM corrections WHERE user_id = user_id;
-
+  SELECT SUM(score) / COUNT(score) INTO average FROM corrections WHERE user_id = input_id;
   -- store the average score for a student
-  UPDATE users SET average_score = average_score WHERE id = user_id;
+  UPDATE users SET average_score = average  WHERE id = input_id;
 END //
+
 DELIMITER ;
