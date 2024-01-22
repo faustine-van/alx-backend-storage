8985b7a613d8f3b3ff26a523ee5d4e4ca1dc8f24
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-22 13:58:50 +0200

    updated

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""inserts a new document in a
+   collection based on kwargs
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+   """ function that inserts a new document in a collection
+       based on kwargs:
+       Returns the new _id
+   """
+   for key, val in ennumerate(kwargs):
+      mongo_collection.insert_many({key: val})
