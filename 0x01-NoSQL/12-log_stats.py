2db05d1129dfeb400f3142dca2ea64c73d4c8deb
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-23 10:29:27 +0200

    updated

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" script that provides some stats
+    about Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
+
+
+if __name__="__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx    
+
