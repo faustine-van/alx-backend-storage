7f3ae0e7c51102e0d82a6cce109d021c82c4606c
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-24 14:20:25 +0200

    update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,6 +15,26 @@ class Cache:
 
     def store(self, data: Union[str, bytes, float, int]) -> str:
         """Generate a random key and store data in the cache."""
-        key: str = str(uuid.uuid4())
+        key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+    def get(self, key: str,
+            fn: Optional[Callable[[bytes], Union[str, float, int]]] = None
+            ) -> Union[str, float, int, None]:
+        """Retrieve data from the cache."""
+        res = self._redis.get(key)
+        if res is None:
+            return None
+        if fn is not None:
+            return fn(res)
+        return res
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Get string from the cache."""
+        res = self.get(key, fn=lambda x: x.decode())
+        return res
+
+    def get_in(self, key: str) -> Optional[int]:
+        """Get integers from the cache."""
+        res = self.get(key, fn=lambda x: int(x.decode()))
+        return res
