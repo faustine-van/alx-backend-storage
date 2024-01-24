124573fb4e0a03a248f359483c544506a55df3fc
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-24 14:10:02 +0200

    update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,7 +13,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, float, int]) -> str:
+    def store(self, data: Union[str, bytes, float, int]) -> str:
         """Generate a random key and store data in the cache."""
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
