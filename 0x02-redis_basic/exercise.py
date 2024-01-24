7a1d7b3d69483c4d96e7dcb8db83d8196c773faf
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-24 14:03:32 +0200

    update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,6 +9,7 @@ class Cache:
     """Cache class"""
 
     def __init__(self, flushdb: bool = False):
+        """declare"""
         self._redis = redis.Redis()
         if flushdb:
             self._redis.flushdb()
@@ -18,24 +19,3 @@ class Cache:
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-
-    def get(self, key: str,
-            fn: Optional[Callable[[bytes], Union[str, float, int]]] = None
-            ) -> Union[str, float, int, None]:
-        """Retrieve data from the cache."""
-        res = self._redis.get(key)
-        if res is None:
-            return None
-        if fn is not None:
-            return fn(res)
-        return res
-
-    def get_str(self, key: str) -> Optional[str]:
-        """Get string from the cache."""
-        res = self.get(key, fn=lambda x: x.decode())
-        return res
-
-    def get_in(self, key: str) -> Optional[int]:
-        """Get integers from the cache."""
-        res = self.get(key, fn=lambda x: int(x.decode()))
-        return res
